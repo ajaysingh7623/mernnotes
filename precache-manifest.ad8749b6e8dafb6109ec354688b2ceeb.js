2caaa9077d0b3252b529b7d801d76decbe42c93d
unknown <you@example.com>
2020-07-30 07:32:57 -0700

    Updates

---
+++ precache-manifest.ad8749b6e8dafb6109ec354688b2ceeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4074f12fe3c2bbf3239c72a343f7d98",
+    "url": "/mernnotes/index.html"
+  },
+  {
+    "revision": "3e0f82dbce5581ed9615",
+    "url": "/mernnotes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2b264178e0280c68cb87",
+    "url": "/mernnotes/static/css/main.8cb5dae3.chunk.css"
+  },
+  {
+    "revision": "3e0f82dbce5581ed9615",
+    "url": "/mernnotes/static/js/2.b84b0b18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mernnotes/static/js/2.b84b0b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b264178e0280c68cb87",
+    "url": "/mernnotes/static/js/main.07840201.chunk.js"
+  },
+  {
+    "revision": "c6e433e389a94434b3c0",
+    "url": "/mernnotes/static/js/runtime-main.1f022ebf.js"
+  }
+]);
